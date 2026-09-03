baf854d5f47bae881002a98d140e0af592d5410c
Gustavo Alonso <gn503536@gmail.com>
2026-09-03 15:16:14 -0600

    UPDATE Unidad2

---
+++ Unidad2/03_For.py
@@ -0,0 +1,19 @@
+for numero in range(0,7,3):
+    cuadrado = numero**2
+    print(numero, cuadrado)
+
+"""
+materias = ["Python", "Linux", "Interfaces"]
+for posicion, materia in enumerate(materias, start=1):
+    print(f"{posicion}. {materia}")
+
+materias = ["Python", "Linux", "Interfaces"]
+for materia in materias:
+    print(materia)
+cadena="0123456789ABCDEF"
+for letra in cadena:
+    print(letra)
+
+for i in range(len(cadena)):
+    print(cadena[i])
+"""
